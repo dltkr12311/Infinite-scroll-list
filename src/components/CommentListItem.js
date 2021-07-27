75c4496b721a90d806c05a227ab52169ca2f6d1d
yoonhe <yhehe228@gmail.com>
2021-07-27 16:29:00 +0900

    style : CommentListItem 스타일 추가

--- src/components/CommentListItem.js
+++ src/components/CommentListItem.js
@@ -1,23 +1,52 @@
 import React from "react";
 
+import styled from "@emotion/styled";
+
+const ItemWrap = styled.li`
+  padding: 20px;
+  background: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  border-radius: 20px;
+
+  & + & {
+    margin-top: 14px;
+  }
+
+  p + p {
+    margin-top: 12px;
+  }
+`;
+
+const Item = styled.p`
+  & > * {
+    font-size: 18px;
+  }
+
+  em {
+    margin-right: 12px;
+    font-weight: bold;
+    display: ${({ lineBreak }) => lineBreak && "block"};
+  }
+`;
+
 const CommentListItem = ({ comment }) => {
   const { postId, email, body } = comment;
 
   return (
-    <li>
-      <p>
+    <ItemWrap>
+      <Item>
         <em>Comment Id</em>
         <span>{postId}</span>
-      </p>
-      <p>
+      </Item>
+      <Item>
         <em>Email</em>
         <span>{email}</span>
-      </p>
-      <p>
+      </Item>
+      <Item lineBreak>
         <em>Comment</em>
         <span>{body}</span>
-      </p>
-    </li>
+      </Item>
+    </ItemWrap>
   );
 };
 
